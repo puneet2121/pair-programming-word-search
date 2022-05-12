52fe2945e23a3a16bebfbd9e29a9ceb12441ca1b
puneet2121 <73667955+puneet2121@users.noreply.github.com>
2022-05-12 00:10:40 -0600

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let newArr = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let subArr = [];
+    for (let j = 0; j < matrix.length; j++) {
+      subArr.push(matrix[j][i]);
     }
-}
-
-module.exports = wordSearch
+    newArr.push(subArr);
+  }
+  return newArr;
+};
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verticleJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticleJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+  
+module.exports = wordSearch;
